2467cfd8672ab1fd4e0061fe54e74d85735159e6
dayis <dayisaacm81@gmail.com>
2024-01-31 13:56:46 -0700

    very first push!!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// Mission 4 // Section 4 Group 1
 
+using System;
+
+public class BoardInfo
+{
+    public void PrintBoard(char[] board)
+    {
+
+    }
+
+    public void Winner(char[] board)
+    {
+
+    }
+}
