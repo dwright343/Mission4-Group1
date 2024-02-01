1aa0ac4eec6db57a98d4390360aacc2369691c98
dayis <dayisaacm81@gmail.com>
2024-02-01 16:55:00 -0700

    created board array and started GetPlayerMove method

--- Program.cs
+++ Program.cs
@@ -1,16 +1,41 @@
 ﻿// Mission 4 // Section 4 Group 1
 
+// HEAD
 using System;
 
-public class BoardInfo
+public class Driver
 {
-    public void PrintBoard(char[] board)
-    {
+    int currPlayer = 1;
 
+    static void Main()
+    {
+        //declare variables
+        char[][] board = new char[3][];
+
+        //initialize board array
+        board[0] = new char[] {' ', ' ', ' '};
+        board[1] = new char[] {' ', ' ', ' '};
+        board[2] = new char[] {' ', ' ', ' '};
+
+        //welcome message
+        Console.WriteLine("Welcome to Tic Tac Toe!");
     }
 
-    public void Winner(char[] board)
+    public void GetPlayerMove()
     {
+        //set up variables
+        string choice = " ";
+        int choiceLocation = 0;
+        bool isValidLocation;
+
+        Console.WriteLine("Player " + currPlayer + ", enter your choice of X or O:");
+        choice = Console.ReadLine().ToUpper();
+        Console.WriteLine("Player " + currPlayer + ", enter your the location of your choice:");
+        isValidLocation = int.TryParse(Console.ReadLine(), out choiceLocation);
 
     }
+    
 }
+//=======
+//Change
+//>>>>>>> 10a570c330b6768d9dab01679a09c11d341b49f3
